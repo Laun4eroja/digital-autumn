59d6bac0436136fcc37396d8bf358e38b2647d66
flash of flame <71262115+Laun4eroja@users.noreply.github.com>
2026-09-15 09:27:23 +0300

    comswiper

--- js/main.js
+++ js/main.js
@@ -126,10 +126,10 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 3,
     spaceBetween: 30,
     loop: true,
-    autoplay: {
-        delay: 7000,
-        disableOnInteraction: false,
-    },
+    // autoplay: {
+    //     delay: 7000,
+    //     disableOnInteraction: false,
+    // },
 
     pagination: {
         el: '.swiper-pagination',
